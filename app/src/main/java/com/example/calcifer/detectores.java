9c59d277c5b4c8b491ee199b20a9c1d6000666c2
caamanomarian@gmail.com <caamanomarian@gmail.com>
2023-11-30 17:23:37 -0600

    Initial commit

---
+++ app/src/main/java/com/example/calcifer/detectores.java
@@ -0,0 +1,14 @@
+package com.example.calcifer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detectores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detectores);
+    }
+}
