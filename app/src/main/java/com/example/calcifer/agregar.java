fcc05e0e1d87e43eac0b4eaeb20da48e2f97703e
caamanomarian@gmail.com <caamanomarian@gmail.com>
2023-12-07 23:25:03 -0600

    Agregamos ID

--- app/src/main/java/com/example/calcifer/agregar.java
+++ app/src/main/java/com/example/calcifer/agregar.java
@@ -3,12 +3,29 @@ package com.example.calcifer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.lang.ref.Reference;
 
 public class agregar extends AppCompatActivity {
+
+    EditText txtNombre;
+    Button btnAgregarD;
+    Spinner spnArea;
+    Spinner spnSala;
+    Spinner spnTipo;
+    FirebaseDatabase db;
+    Reference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar);
+
+
     }
 }
